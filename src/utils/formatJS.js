2a65ac616976640aba826654b3a5ea856449c16e
Slava <visla.vvi@gmail.com>
2022-05-17 11:58:59 +0300

    Pass parser 'flow' to prettier.format.

--- src/utils/formatJS.js
+++ src/utils/formatJS.js
@@ -2,7 +2,7 @@
 const prettier = require('prettier');
 
 function formatJS(str: string): string {
-  return prettier.format(str);
+  return prettier.format(str, {parser: 'flow'});
 }
 
 module.exports = formatJS;
